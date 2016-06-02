65681f72c7a0d4b60b75d173c0a0e163a8213fbc
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-06-02 07:31:28 -0400

    Changing from responsive to fixed skyscraper ads to avoid banners.

--- src/Web/src/layout.tsx
+++ src/Web/src/layout.tsx
@@ -8,19 +8,17 @@ function Layout(props: any) {
         <div className='container-fluid'>
             <div className='row'>
                 <div className='col-sm-5 col-md-4'>
-                    <Ad slot='9313090625'/>
+                    <Ad slot='4400369824' width={160} height={600}/>
                 </div>
                 <div className='col-sm-2 col-md-4'>
                     <Home {...props}/>
                 </div>
                 <div className='col-sm-5 col-md-4'>
-                    <Ad slot='1789823825'/>
+                    <Ad slot='5737502220' width={160} height={600}/>
                 </div>
             </div>
         </div>
     );
 }
-
-// <Ad slot='6220023426' width:728 height:90/>
 
 export default connect(x => x)(Layout);
