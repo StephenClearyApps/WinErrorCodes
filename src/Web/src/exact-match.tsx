58a7327e4e6e3e5f1a8a1f6e5dd220256d031836
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-06-02 07:27:23 -0400

    Fix weird import.

--- src/Web/src/exact-match.tsx
+++ src/Web/src/exact-match.tsx
@@ -1,13 +1,10 @@
 ﻿import React from 'react';
-import * as dataTypings from './typings/data';
-import { ErrorMessage } from './typings/data';
-
-// Weird dataTypings import and any cast: https://github.com/Microsoft/TypeScript/issues/3422
+import { ErrorMessage, errorMessageTypeHumanReadableString } from './typings/data';
 
 function ExactMatch({ errorMessage }: { errorMessage: ErrorMessage }) {
     return (
         <div>
-            <div>Type: {(dataTypings as any).ErrorMessageType[errorMessage.type]}</div>
+            <div>Type: {errorMessageTypeHumanReadableString(errorMessage.type)}</div>
             <div>{errorMessage.identifiers.map(x => <div key={x}><code>{x}</code></div>) }</div>
             <div>{errorMessage.text}</div>
         </div>
