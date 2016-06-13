dd1afebc19e0e71b8c1bf48c54663b6aa1f291d5
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-06-13 06:50:32 -0400

    Add navbar.

--- src/Web/src/layout.tsx
+++ src/Web/src/layout.tsx
@@ -1,20 +1,41 @@
 ﻿import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import Home from './home';
 import Ad from './ad';
 
 function Layout(props: any) {
     return (
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-sm-5 col-md-4'>
-                    <Ad slot='7090994222' width={300} height={600}/>
+        <div>
+            <nav className="navbar navbar-default navbar-fixed-top">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <Link className="navbar-brand" to="/">ErrorCodeLookup.com</Link>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><a href="http://stephencleary.com/">Author</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className='col-sm-2 col-md-4'>
-                    <Home {...props}/>
-                </div>
-                <div className='col-sm-5 col-md-4'>
-                    <Ad slot='1044460625' width={300} height={600}/>
+            </nav>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-sm-5 col-md-4'>
+                        <Ad slot='7090994222' width={300} height={600}/>
+                    </div>
+                    <div className='col-sm-2 col-md-4'>
+                        <Home {...props}/>
+                    </div>
+                    <div className='col-sm-5 col-md-4'>
+                        <Ad slot='1044460625' width={300} height={600}/>
+                    </div>
                 </div>
             </div>
         </div>
